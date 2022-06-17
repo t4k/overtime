d6a61cf7746f46def405ed8c63532f9912ae2335
Tommy Keswick <t4k@users.noreply.github.com>
2022-06-16 18:06:12 -0700

    replace requests-html with playwright

--- posts.py
+++ posts.py
@@ -2,15 +2,8 @@
 
 import sys
 
-import urllib3
-
 from bs4 import BeautifulSoup
-from requests_html import HTMLSession
-
-# InsecureRequestWarning: Unverified HTTPS request is being made to host. Adding
-# certificate verification is strongly advised. See:
-# https://urllib3.readthedocs.io/en/1.26.x/advanced-usage.html#ssl-warnings
-urllib3.disable_warnings()
+from playwright.sync_api import sync_playwright
 
 if len(sys.argv) > 1:
     # expected format: owner/repository
@@ -19,39 +12,37 @@ if len(sys.argv) > 1:
 else:
     sys.exit("owner/repository argument is required")
 
-session = HTMLSession()
-if len(sys.argv) > 2:
-    # argument should be a URL with post links
-    posts = session.get(sys.argv[2])
-else:
-    # default; posts.html contains a blog widget that returns a list of posts
-    posts = session.get(f"https://{owner}.github.io/{repository}/posts.html")
-
-# render() allows the blog widget code to run and return a list of posts
-posts.html.render()
-
-for link in posts.html.links:
-    s = HTMLSession(verify=False)
-    blogpost = s.get(link, verify=False)
-    # render blogpost with javascript
-    blogpost.html.render()
-    # remove content of dynamic elements
-    soup = BeautifulSoup(blogpost.html.html, "html.parser")
-    soup.find(class_="s-lg-blog-recent-posts").clear()
-    soup.find(class_="s-lg-blog-archive-list").clear()
-    # write prettified html to file
-    with open(f'{repository}/posts/{link.split("/")[-1]}.html', "w") as file:
-        file.write(soup.prettify(formatter="minimal"))
-    # NOTE blog code contains incrementing timestamps to be zeroed-out
-    lines = []
-    with open(f'{repository}/posts/{link.split("/")[-1]}.html', "r") as file:
-        for line in file:
-            if "time:" in line:
-                start = line.split("time:", 1)[0]
-                end = line.split("time:", 1)[-1].split(",", 1)[-1]
-                lines.append(f"{start}time: 0{end}")
-            else:
-                lines.append(line)
-    # write modified html to file
-    with open(f'{repository}/posts/{link.split("/")[-1]}.html', "w") as file:
-        file.writelines(lines)
+with sync_playwright() as playwright:
+    b = playwright.firefox.launch()
+    p = b.new_page()
+    if len(sys.argv) > 2:
+        # argument should be a URL with blogpost links
+        p.goto(sys.argv[2])
+    else:
+        # default; posts.html contains a blog widget that returns a list of posts
+        p.goto(f"https://{owner}.github.io/{repository}/posts.html")
+    links = p.eval_on_selector_all("a", "elements => elements.map(element => element.href)")
+    for link in links:
+        p.goto(link)
+        # remove content of dynamic elements
+        soup = BeautifulSoup(p.content(), "html.parser")
+        soup.find(class_="s-lg-blog-recent-posts").clear()
+        soup.find(class_="s-lg-blog-archive-list").clear()
+        filename = f'{repository}/posts/{link.split("/blog/")[-1].replace("/", "_")}.html'
+        # write prettified html to file
+        with open(filename, "w") as file:
+            file.write(soup.prettify(formatter="minimal"))
+        # NOTE blog code contains incrementing timestamps to be zeroed-out
+        lines = []
+        with open(filename, "r") as file:
+            for line in file:
+                if "time:" in line:
+                    start = line.split("time:", 1)[0]
+                    end = line.split("time:", 1)[-1].split(",", 1)[-1]
+                    lines.append(f"{start}time: 0,{end}")
+                else:
+                    lines.append(line)
+        # write modified html to file
+        with open(filename, "w") as file:
+            file.writelines(lines)
+    b.close()
