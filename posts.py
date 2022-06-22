335647af63767007acb00a3b12cc750d27bc6ca8
Tommy Keswick <tkeswick@caltech.edu>
2022-06-22 09:18:52 -0700

    refactor blogpost scraping to use pagination; close #3

--- posts.py
+++ posts.py
@@ -1,43 +1,49 @@
-# Scrape LibGuides blog posts to track changes.
-
 import re
-import sys
+import time
 
 from bs4 import BeautifulSoup
 from playwright.sync_api import sync_playwright
 
-if len(sys.argv) > 1:
-    # expected format: owner/repository
-    owner = sys.argv[1].split("/")[0]
-    repository = sys.argv[1].split("/")[-1]
-else:
-    sys.exit("owner/repository argument is required")
+def main(
+    site_base_url: "Base URL of LibGuides website",  # type: ignore
+    content_repository_name: "Repository name where scraped HTML files will be committed",  # type: ignore
+):
+    """Scrape LibGuides blog posts to track changes."""
+    with sync_playwright() as playwright:
+        b = playwright.firefox.launch()
+        p = b.new_page(base_url=site_base_url)
 
-with sync_playwright() as playwright:
-    b = playwright.firefox.launch()
-    p = b.new_page()
+        p.goto(f"/blog")
+        links = []
+        while True:
+            # NOTE easier to wait 1 second than solve the issues that arose
+            # waiting for the elements with playwright methods
+            time.sleep(1)
+            blog = BeautifulSoup(p.content(), "html.parser")
+            links.extend(blog.select(".s-lg-blog-header a"))
+            if blog.find(class_="next"):
+                if blog.find("a", text="Older Posts"):
+                    p.click(".next a:text('Older Posts')")
+                else:
+                    break
+            else:
+                break
 
-    if len(sys.argv) > 2:
-        # argument should be a URL with blogpost links
-        p.goto(sys.argv[2])
-    else:
-        # default; posts.html contains a blog widget that returns a list of posts
-        p.goto(f"https://{owner}.github.io/{repository}/posts.html")
+        for link in links:
+            p.goto(link["href"])
+            soup = BeautifulSoup(p.content(), "html.parser")
+            # select blogpost content only
+            blogpost = soup.find(id=re.compile("^comment-\d+$"))
+            filename = (
+                f'{content_repository_name}/posts/{link["href"].split("/blog/")[-1].replace("/", "_")}.html'
+            )
+            # write prettified html to file
+            with open(filename, "w") as file:
+                file.write(blogpost.prettify())
 
-    links = p.eval_on_selector_all(
-        "a", "elements => elements.map(element => element.href)"
-    )
+        b.close()
 
-    for link in links:
-        p.goto(link)
-        soup = BeautifulSoup(p.content(), "html.parser")
-        # select blogpost content only
-        blogpost = soup.find(id=re.compile("^comment-\d+$"))
-        filename = (
-            f'{repository}/posts/{link.split("/blog/")[-1].replace("/", "_")}.html'
-        )
-        # write prettified html to file
-        with open(filename, "w") as file:
-            file.write(blogpost.prettify())
 
-    b.close()
+if __name__ == "__main__":
+    # fmt: off
+    import plac; plac.call(main)
